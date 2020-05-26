00a153c1dbddd35a79f5e414fe2d353e491a3192
systex-admin <59816881+systex-admin@users.noreply.github.com>
2020-05-26 15:59:23 +0800

    Update and rename change_xoops_url.sh to replace_xoops_url.sh

--- replace_xoops_url.sh
+++ replace_xoops_url.sh
@@ -7,11 +7,17 @@ XOOPS_OLD_URL=${3}
 if [ $# -lt 3 ]; then
         echo "usage: ${0} [XOOPS_ROOT_PATH] [XOOPS_CHANGE_URL] [XOOPS_OLD_URL] "
         echo "Example:"
+        echo "(1)"
         echo "${0} /var/www/ www.faes.tyc.edu.tw 10.240.49.101"
+        echo "or"
+        echo "(2)"
+        echo "${0} /var/www/ 10.240.49.101 www.faes.tyc.edu.tw"
+        echo "or"
+        echo "(3)"
+        echo "${0} /var/www/ www.faes.tyc.edu.tw faes.tyc.edu.tw"
         echo
         echo "error: too few arguments"
         exit 1
 fi
 
-#grep -rl www.faes.tyc.edu.tw /var/www/ | xargs sed -i 's/www.faes.tyc.edu.tw/10.240.49.101/g'
 grep -rl ${XOOPS_CHANGE_URL} ${XOOPS_ROOT_PATH} | xargs sed -i "s/${XOOPS_CHANGE_URL}/${XOOPS_OLD_URL}/g"
